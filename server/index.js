8dd4085593e6316a104f921a52246af0dc290a70
devJerb <gutierrez.jeromebryant@gmail.com>
2023-05-12 21:13:19 +0800

    fixed file structure; separated front and back end

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import epxress from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello World!' });
+})
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => {
+            console.log('Server started on port http://localhost:8080');
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
